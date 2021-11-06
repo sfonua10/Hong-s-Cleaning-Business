88490ec747a5f87a0a4d4cf02880d9bae0728552
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2021-11-06 07:40:31 -0600

    work in progress

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
-import Head from 'next/head'
-import Example from '../components/Example'
+import Head from "next/head";
+import Example from "../components/Example";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hong's Cleaning Business</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Hong's Cleaning Business" />
+        <meta
+          property="og:description"
+          content="Offering quality cleaning services"
+        />
+        <meta
+          property="og:image"
+          content="https://images.pexels.com/photos/6195277/pexels-photo-6195277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        />
+        <meta
+          property="og:url"
+          content="https://hong-s-cleaning-business.vercel.app/"
+        ></meta>
       </Head>
       <Example />
 
@@ -22,5 +35,5 @@ export default function Home() {
         </a>
       </footer> */}
     </div>
-  )
+  );
 }
